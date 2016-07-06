d7a89f7934b71ba9f5cfe08bfa55942fddc66062
John Sigmon <johnasigmon@gmail.com>
2016-07-06 08:59:23 -0400

    adjustments

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,12 @@
 $(document).ready(function() {
 
   $('.saveArticle').on('click', function(event){
-   const clicked =  $(this).parent().parent().html()
+   // const clicked =  $(this).parent().parent().html()
+   let cardDiv = $(this).closest('card')
+   let span = cardDiv.attr('span')
+   let pText = cardDiv.attr('p')
+
+   console.log('Span :', span, 'P content :', pText)
    const dataObj = {
     company: clicked,
   }
